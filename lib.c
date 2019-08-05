6bd5910f58c0c035b095d79029f67fc682f4596c
Funkschy <felixschoeller@outlook.de>
2019-08-05 22:34:35 +0200

    added identifers and token printing

--- lib.c
+++ lib.c
@@ -4,10 +4,21 @@
 
 #define HASH_NUM 65599
 
+static char *const token_strings[] = {
+    "error",
+    "integer",
+    "identifier",
+    "eof"
+};
+
 int32_t hash(char const *key) {
     int32_t h = 0;  
     for (char c = *key; c != '\0'; c++) {
         h = h * HASH_NUM + c;
     }
     return h;
+}
+
+char const* const tok2str(int32_t type) {
+    return token_strings[type];
 }
