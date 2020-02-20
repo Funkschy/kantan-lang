059bb8d2643703485727f4cca81338e8721a70e2
Funkschy <felix.schoeller@protonmail.com>
2020-02-20 14:06:15 +0100

    fixed segfault in return type checking

--- test/files/modules.py
+++ test/files/modules.py
@@ -1,5 +1,7 @@
+from typing import List
 from testcase import Code
 
 
 class Test(Code):
-	pass
+    def files(self) -> List[str]:
+        return list(map(lambda f: self.relative_to(f), ['modules/main.kan', 'modules/lib.kan', 'modules/bintree.kan']))
