8c3cf170efa1164195a4b1e2f4576df1e4b35c10
Funkschy <felix.schoeller@protonmail.com>
2020-05-28 20:05:39 +0200

    added valgrind suppression file to kantan-test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # runs the kantan tests
 
-python3 ./kantan-test/src/main.py $(realpath ../compiler) $(realpath ./files) --valgrind=True
+python3 ./kantan-test/src/main.py $(realpath ../compiler) $(realpath ./files) --valgrind=True --suppress=$(realpath ../suppress-llvm-errors.supp)
+rm out.o
