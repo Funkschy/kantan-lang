0143c9db578e8e8bfb8046471a9e21641b21399a
Funkschy <felix.schoeller@protonmail.com>
2020-07-04 14:56:31 +0200

    added integer globals

--- etc/generate-llvm-bindings.py
+++ etc/generate-llvm-bindings.py
@@ -66,6 +66,7 @@ whitelist = set([
     'LLVMBuildSub',
     'LLVMConstInt',
     'LLVMConstIntOfString',
+    'LLVMConstIntOfStringAndSize',
     'LLVMConstNull',
     'LLVMConstRealOfString',
     'LLVMConstStringInContext',
